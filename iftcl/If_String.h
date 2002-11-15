b27ae0b4bdb172f73e5e1c1e28d1e30dacac86a2
rfranke <rfranke>
2002-11-15 07:42:54 +0000

    introduce If_String for const char *

--- iftcl/If_String.h
+++ iftcl/If_String.h
@@ -1,14 +1,12 @@
 /*
  *  If_String.h
- *     - If_Variable for C++ string type
- *       (Everything is inlined so that only programs that use it
- *        have to link a Std C++ library.)
+ *     If_Variable for C strings (const char *)
  *
  *  rf, 3/17/01
  */
 
 /*
-    Copyright (C) 1994--2001  Ruediger Franke
+    Copyright (C) 1994--2002  Ruediger Franke
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -31,130 +29,26 @@
 
 #include "If_Variable.h"
 
-#include <string>
+/** Interface type for C string */
+typedef const char * If_String_t;
 
-typedef std::string If_String_t;
-
-// callback for write-access
-//--------------------------
-class If_StringWriteIf {
-
- public:
-  virtual ~If_StringWriteIf() {}
-  virtual int write(If_String_t newVal)=0;
-};
-
-template <class X>
-class If_StringWriteCB: public If_StringWriteIf {
+/** Interface variable of C string type. */
+class IF_API If_String: public If_Variable<If_String_t> {
 
  protected:
-  X	*_object;
-  int	(X::*_write)(If_String_t);
+  // conversion of internal data from and to a Tcl object
+  int getTclObj(Tcl_Interp *);
+  int setTclObj(Tcl_Interp *, Tcl_Obj *CONST objPtr);
 
  public:
-  If_StringWriteCB(int (X::*n_write)(If_String_t), X *n_object)
-    {
-      assert(n_write != NULL && n_object != NULL);
-      _write = n_write;
-      _object = n_object;
-    }
-  int write(If_String_t newVal)
-    {
-      return (_object->*_write)(newVal);
-    }
+  /** Constructor taking callback methods as arguments. */
+  If_String(const char *ifName, If_GetIf<If_String_t> *getCb,
+	    If_SetIf<If_String_t> *setCb = NULL)
+    :If_Variable<If_String_t>(ifName, getCb, setCb) {}
+
+  /** Alternative constructor for direct read access through a pointer. */
+  If_String(const char *ifName, If_String_t *varPtr)
+    :If_Variable<If_String_t>(ifName, new If_GetPt<If_String_t>(varPtr)) {}
 };
-
-// class declaration
-//------------------
-class If_String: public If_Variable {
-
- protected:
-  If_String_t		*_varPtr;
-  If_StringWriteIf	*_callback;
-
-  // define abstract methods of If_Variable
-  //---------------------------------------
-  int                  put(Tcl_Obj *CONST objPtr);
-  int                  get();
-
- public:
-  If_String(const char *ifName, If_String_t *varPtr,
-	    const char *mode = "rw");
-  If_String(const char *ifName, If_String_t *varPtr,
-	    If_StringWriteIf *callback);
-  ~If_String();
-};
-
-/*
- *  If_String.C -- class definition
- */
-
-//--------------------------------------------------------------------------
-If_String::If_String(const char *ifName, If_String_t *varPtr,
-		     const char *mode)
-  :If_Variable(ifName, mode)
-{
-  _varPtr = varPtr;
-  _callback = NULL;
-}
-
-//--------------------------------------------------------------------------
-If_String::If_String(const char *ifName, If_String_t *varPtr,
-		     If_StringWriteIf *callback)
-  :If_Variable(ifName)
-{
-  _varPtr = varPtr;
-  _callback = callback;
-}
-
-//--------------------------------------------------------------------------
-If_String::~If_String()
-{
-  delete _callback;
-}
-
-//--------------------------------------------------------------------------
-int If_String::put(Tcl_Obj *CONST objPtr)
-{
-  char *str_value;
-  int str_length;
-
-  // parse the new value
-  //--------------------
-  str_value = Tcl_GetStringFromObj(objPtr, &str_length);
-  if (str_value == NULL)
-    return TCL_ERROR;
-
-  If_String_t value(str_value);
-
-  // use the new value
-  //------------------
-  if (_callback) {
-    if (_callback->write(value) != IF_OK) {
-      Tcl_AppendResult(theInterp, "writing of ", _ifName, " rejected", NULL);
-      return TCL_ERROR;
-    }
-  }
-  else
-    *_varPtr = value;
-
-  return TCL_OK;
-}
-
-//--------------------------------------------------------------------------
-int If_String::get()
-{
-  // (need to cast to char* as Tcl does not use const qualifier)
-  Tcl_Obj *objPtr = Tcl_NewStringObj((char *)(*_varPtr).c_str(),
-				     (*_varPtr).length());
-
-  Tcl_SetObjResult(theInterp, objPtr);
-
-  return TCL_OK;
-}
-
-
-//==========================================================================
-
 
 #endif
