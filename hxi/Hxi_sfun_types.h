0ae66757ac2e8f741a9470a55d7399fc6932a42d
rfranke <rfranke>
2005-06-14 20:28:00 +0000

    treat files to build Hxi S-function model outside Hqp solver

--- hxi/Hxi_sfun_types.h
+++ hxi/Hxi_sfun_types.h
@@ -8,25 +8,6 @@
  * rf, 05/06/2001
  */
 
-/*
-    Copyright (C) 1994--2005  Ruediger Franke
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; 
-    version 2 of the License.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library (file COPYING.LIB);
-    if not, write to the Free Software Foundation, Inc.,
-    59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 /** Avoid multiple inclusion */
 #if !defined(Hxi_sfun_types_H)
 #define Hxi_sfun_types_H
