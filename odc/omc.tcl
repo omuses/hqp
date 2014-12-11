04b38e693e100739c7b63f442c6f7cdf9603db02
Rüdiger Franke <rdgfranke@gmail.com>
2014-12-11 22:10:51 +0100

    Correct call to omc for FMU generation   - support multi-line error messages   - force copy of result

--- odc/omc.tcl
+++ odc/omc.tcl
@@ -9,14 +9,14 @@ set omc {omc}
 proc compileFMU {mo_file} {
     global omc
     set model_name [file rootname $mo_file]
-	set mo_file_normalized [file normalize $mo_file]
-	# create a temporary directory
-	set cwd [pwd]
-	if {![file exists tmp]} {
-		file mkdir tmp
-	}
-	cd tmp
-	# create script file for omc
+    set mo_file_normalized [file normalize $mo_file]
+    # create a temporary directory
+    set cwd [pwd]
+    if {![file exists tmp]} {
+	file mkdir tmp
+    }
+    cd tmp
+    # create script file for omc
     set fp [open "omc_tcl_commands.mos" w]
     puts $fp "// temporary file with omc commands"
     puts $fp "loadFile(\"$mo_file_normalized\");"
@@ -24,19 +24,22 @@ proc compileFMU {mo_file} {
     puts $fp "translateModelFMU($model_name, version=\"2.0\");"
     puts $fp "getErrorString();"
     close $fp
-	# call omc, check for errors and clean up
+    # call omc, check for errors and clean up
     if {[catch {eval [concat exec $omc "omc_tcl_commands.mos"]} log]} {
-		cd $cwd
-		error "Could not compile FMU: $log"
-	}
 	cd $cwd
-    if {[lindex $log 1] != ""} {
-		error [lindex $log 1]
+	error "Could not compile FMU: $log"
     }
-    if {[lindex $log 3] != ""} {
-		error [lindex $log 3]
+    cd $cwd
+    set logLines [split $log "\n"]
+    set loadFile [lindex $logLines 1]
+    if {$loadFile != {""}} {
+	error $loadFile
     }
-	file copy "tmp/$model_name.fmu" .
+    set translateModelFMU [join [lrange $logLines 3 end] "\n"]
+    if {$translateModelFMU != {""}} {
+	error $translateModelFMU
+    }
+    file copy -force "tmp/$model_name.fmu" .
     file delete -force tmp
     return [lindex $log 2]
 }
